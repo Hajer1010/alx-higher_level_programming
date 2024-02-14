30118f706906dee9cb2c532a9e9bb79bf99340a4
Hajer1010 <hajeer_00@hotmail.com>
2024-02-14 13:15:07 -0800

    done

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,6 +1,6 @@
--- a script display max tempretature of  state
-SELECT state, MAX(value) AS max_temp
-FROM temperatures
-GROUP BY state
-ORDER BY state;
-
+-- A script that lists all shows contained in the database hbtn_0d_tvshows
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres
+ON tv_show_genres.show_id = tv_shows.id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,7 @@
--- a script that lists all the cities of California 
+-- A script that lists all the cities of California
 SELECT id, name
 FROM cities
-WHERE state_id IN(
-	SELECT id FROM states WHERE name = 'California');
+WHERE state_id IN (SELECT id
+      	    FROM states
+	    WHERE name = 'California')
 ORDER BY id ASC;
