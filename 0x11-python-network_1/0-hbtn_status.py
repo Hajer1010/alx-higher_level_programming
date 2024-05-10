252df8afd8fc73de41194b28377f85ece15c8f2e
Hajer1010 <hajeer_00@hotmail.com>
2024-05-10 07:20:27 -0700

    done

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-"""Fetches https://alx-intranet.hbtn.io/status."""
-from urllib.request import urlopen
+"""Fetches https://alx-intranet.hbtn.io/status"""
+
 
 if __name__ == "__main__":
-    url = 'https://alx-intranet.hbtn.io/status'
-    with urlopen(url) as response:
-        html_bytes = response.read()
-        html = html_bytes.decode()
-        print("- " + html.replace("\n", "\n\t-"))
+    import urllib.request
+
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        html = res.read()
+        print('Body response:')
+        print('\t- type: {}'.format(type(html)))
+        print('\t- content: {}'.format(html))
+        print('\t- utf8 content: {}'.format(html.decode("utf-8", "replace")))
